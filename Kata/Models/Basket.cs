5ed3274e6685b8664204487496421e89f4cd7995
callieSed <78051369+callieSed@users.noreply.github.com>
2021-01-26 22:13:08 +0000

    Added Initial Files
    Created basket and basket item class

---
+++ Kata/Models/Basket.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace Kata.Models
+{
+    public class Basket: List<BasketItem>
+    {
+        public new void Add(BasketItem item)
+        {
+            var existingItem = this.Any(a => a.ItemName == item.ItemName);
+            if (existingItem)
+            {
+                this.First(a => a.ItemName == item.ItemName).Count++;
+            }
+            else
+            {
+                Add(item);
+            }
+        }
+
+        public new void Remove(BasketItem item)
+        {
+            var existingItem = this.First(a => a.ItemName == item.ItemName);
+            if (item.Count > 0)
+                item.Count--;
+        }
+
+        public double TotalPrice()
+        {
+            double totalSum = 0;
+            foreach (var item in this.Where(a => a.Count > 0))
+            {
+                totalSum += item.GetTotalPrice();
+            }
+
+            return totalSum;
+        }
+    }
+}
+
+
---
+++ Kata/Models/BasketItem.cs
@@ -0,0 +1,13 @@
+﻿namespace Kata.Models
+{
+    public class BasketItem
+    {
+        public string ItemName { get; set; }
+        public double Price { get; set; }
+        public int Count { get; set; } = 0;
+        public double DiscountPercent { get; set; } = 0;
+        public int DiscountMuliBuy { get; set; } = 0;
+        public int DiscountTotal { get; set; } = 0;
+
+    }
+}
