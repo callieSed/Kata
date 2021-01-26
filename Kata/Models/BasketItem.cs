a1edf1561f93950543e4823e0f146c3fbc6b33dc
CallieSed <callieseddon@gmail.com>
2021-01-26 22:22:13 +0000

    Added GetTotalPrice to BasketItem
    Added GetTotalPrice and calculation for discounts

--- Kata/Models/BasketItem.cs
+++ Kata/Models/BasketItem.cs
@@ -9,5 +9,32 @@
         public int DiscountMuliBuy { get; set; } = 0;
         public int DiscountTotal { get; set; } = 0;
 
+        public double GetTotalPrice()
+        {
+            if (DiscountMuliBuy >= Count)
+            {
+                return CalculateDiscount();
+            }
+            return Price * Count;
+        }
+
+        private double CalculateDiscount()
+        {
+            var timesToBeApplied = Count / DiscountMuliBuy;
+            var remainder = Count % DiscountMuliBuy;
+            if (DiscountPercent > 0)
+            {
+                double subTotal = Price * Count;
+                double amountToDiscount = subTotal * DiscountPercent;
+                double totalPriceAfterDiscount = subTotal - (amountToDiscount * timesToBeApplied);
+                return totalPriceAfterDiscount += (remainder * Price);
+            }
+            else if (DiscountTotal > 0)
+            {
+                var totalDiscountSum = timesToBeApplied * DiscountTotal;
+                return totalDiscountSum + (remainder * Price);
+            }
+            return 0;
+        }
     }
 }
